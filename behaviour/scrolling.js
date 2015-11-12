340938e6874825a238b01b26d9f3b35fb32698ae
othman853 <anuar.yasser@gmail.com>
2015-11-12 19:32:47 -0200

    Scrolling is actually smooth between anchors.

--- behaviour/scrolling.js
+++ behaviour/scrolling.js
@@ -1,11 +1,32 @@
 function Scrolling() {
 
+  function findDestinationByEvent(hash) {
+
+    var anchor = event.srcElement;
+
+    var nextSectionId = anchor.hash.replace(/[#]/, '');
+    var nextSection = document.getElementById(nextSectionId);
+
+    return nextSection;
+  }
+
   Scrolling.prototype.smoothOnclickScroll = function _smoothOnclickScroll(event) {
     event.preventDefault();
 
-    var anchor = event.srcElement;
-        
-    window.location = anchor.href;
-  };
+    var nextSection = findDestinationByEvent(event);
 
+    var current = self.pageYOffset;
+    var destination = nextSection.offsetTop;
+
+    var scrollInterval = setInterval(function _onScrollInterval() {
+
+      if (current >= destination) {
+        clearInterval(scrollInterval);
+      }
+
+      window.scrollBy(0, 3);
+      current += 3;
+
+    }, 1);
+  };
 };
